9a0ecc28edf7c13ed4991911e005322f741793e4
mochenya <mochen20070702@gmail.com>
2023-05-13 09:41:16 +0800

    build: Update ZyClang (Clang-17.0.0-20230510)

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 WORKDIR="$(pwd)"
 
 # ZyClang
-ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/17.0.0-20230501-release/Clang-17.0.0-20230501.tar.gz"
+ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/17.0.0-20230510-release/Clang-17.0.0-20230510.tar.gz"
 ZYCLANG_DIR="$WORKDIR/ZyClang/bin"
 
 # Kernel Source
@@ -92,6 +92,7 @@ CXX=clang++ \
 AR=llvm-ar \
 LD=ld.lld \
 STRIP=llvm-strip \
+OBJCOPY=llvm-objcopy \
 OBJDUMP=llvm-objdump \
 OBJSIZE=llvm-size \
 READELF=llvm-readelf \
