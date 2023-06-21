7a0821d1cbe290e8c2ea2eaad738fd2348e47d1f
mochenya <mochen20070702@gmail.com>
2023-06-21 18:04:58 +0800

    build: Update ZyClang (Clang-17.0.0-20230621)

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@
 WORKDIR="$(pwd)"
 
 # ZyClang
-ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/17.0.0-20230617-release/Clang-17.0.0-20230617.tar.gz"
+ZYCLANG_DLINK="https://github.com/ZyCromerZ/Clang/releases/download/17.0.0-20230621-release/Clang-17.0.0-20230621.tar.gz"
 ZYCLANG_DIR="$WORKDIR/ZyClang/bin"
 
 # Kernel Source
